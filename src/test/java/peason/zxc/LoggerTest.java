7199cdd4aa50735d4a89ea88383fbea48acc8262
zxccong <1209252967@qq.com>
2019-06-18 15:09:28 +0800

    first commit

---
+++ src/test/java/peason/zxc/LoggerTest.java
@@ -0,0 +1,22 @@
+package peason.zxc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoggerTest {
+
+    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void test1(){
+        logger.debug("debug...");
+        logger.info("info...");
+        logger.error("error...");
+    }
+}
